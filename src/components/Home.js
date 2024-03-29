da6a66040eb8eaf2c83124366f0e64264dbc9ef3
Ali Hamza <158183663+aliHztech@users.noreply.github.com>
2024-03-29 14:15:33 +0500

    chng button

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,18 +11,18 @@ const Home = () => {
     <>
       <div className="container">
         <h2>Task Manager Web App</h2>
-        {/* <Link to="/create" className="btn btn-success my-3">
-        {" "}
-        create +
-      </Link> */}
-        <button onClick={() => dispatch(fetchTodo())}></button>
+        <button
+          className="btn btn-success my-3"
+          onClick={() => dispatch(fetchTodo())}
+        >
+          Click To Get Usernames
+        </button>
         <table className="table">
           <thead>
             <tr>
               <td>
                 <b>user name</b>
               </td>
-              {/* <td>action</td> */}
             </tr>
           </thead>
           <tbody>
@@ -35,24 +35,6 @@ const Home = () => {
                     <td>
                       <Link to={`/task/${todo.id}`}>{todo.username}</Link>
                     </td>
-
-                    {/* <td>
-                <Link
-                  to={`/edit/${firstData.id}`}
-                  className="btn btn-sm btn-primary"
-                >
-                  Edit
-                </Link>
-                <button
-                  onClick={() => handleDelete(firstData.id)}
-                  className="btn btn-sm btn-danger ms-2"
-                >
-                  Delete
-                </button>
-                <button className="btn btn-sm btn-warning ms-2">
-                  Add Task
-                </button>
-              </td> */}
                   </tr>
                 );
               })
