de5406af1d7a9fcb662d5f72cf3cef7b1ce1ec8e
Ali Hamza <158183663+aliHztech@users.noreply.github.com>
2024-04-16 13:19:48 +0500

    fixes

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Button from "react-bootstrap/Button";
-import ModalEdit from "./ModalEdit";
 import { editTask } from "../redux/TodoSlice";
 const Task = () => {
   const { id } = useParams();
